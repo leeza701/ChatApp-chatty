ee392b54d7389a0798726be0ad61f0fa6d133752
leeza701 <leezamalik789@gmail.com>
2026-02-16 18:10:58 +0530

    fixed error in chatContainer.jsx

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -5,7 +5,7 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? import.meta.env.VITE_API_URL
-      :"http://localhost:5000/api",
+      :"http://localhost:3001/api",
 
   withCredentials: true, // 🚨 CRITICAL
 });

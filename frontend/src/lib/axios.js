c0f706a4d5f3c6d831c7f32e109589c1fd4c7fc1
leeza701 <leezamalik789@gmail.com>
2026-02-16 17:49:04 +0530

    changes in axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "http://localhost:3001/api"
-      : "/api",
+  baseURL: import.meta.env.VITE_API_URL ,
   withCredentials: true,
 });

d5bd58f013b3b4b4c2b397cce4ba69df8070bd1f
leeza701 <leezamalik789@gmail.com>
2026-04-13 12:23:44 +0530

    Fix case-sensitive import for messageSkeleton (Linux/Render compatibility)

--- frontend/src/component/ChatContainer.jsx
+++ frontend/src/component/ChatContainer.jsx
@@ -4,7 +4,7 @@ import { useEffect, useRef } from "react";
 
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./Messageinput";
-import MessageSkeleton from "./skeletons/MessageSkeleton";
+import MessageSkeleton from "./skeletons/messageSkeleton";
 
 import { useAuthStore } from "../store/useAuthStore";
 import { formatMessageTime } from "../lib/utils";
